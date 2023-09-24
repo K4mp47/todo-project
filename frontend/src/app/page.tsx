7d347bbc9df222a399db5b8c6acbeb8c524071d4
K4mp47 <campagnoloalberto5@gmail.com>
2023-09-24 12:58:52 +0200

    first commit

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="m-auto h-full flex flex-col justify-center content-center items-center text-center">
+      <h1 className="mb-6 text-3xl">Welcome Into my To Do App</h1>
+      <h3 className="text-md">Login is Required</h3>
+      <button className="bg-gray-400 w-56 h-12 rounded-xl m-8 z-2">
+        Login 
+      </button>
+    </div>
+  )
+}
