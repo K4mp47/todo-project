5c264e5275bb62f8b86d8a078da8d03d81c9312c
K4mp47 <campagnoloalberto5@gmail.com>
2024-02-09 15:38:29 +0100

    chore: changes

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cabf1e3ab07fde01"; }
+/******/ 		__webpack_require__.h = function() { return "31e66641a92a2cbc"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- frontend/.next/static/webpack/webpack.b717ffd9fe64912f.hot-update.js
+++ frontend/.next/static/webpack/webpack.b717ffd9fe64912f.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "3d039d5748866758"; }
+/******/ 	__webpack_require__.h = function() { return "31e66641a92a2cbc"; }
 /******/ }();
 /******/ 
 /******/ }
